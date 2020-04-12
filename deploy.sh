3755db7c7177c64a4bf355c603fa247a722b7e3a
TME520 <tme520@gmail.com>
2020-04-13 08:28:05 +1000

    Added deploy.sh shell + Created the assets folder

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+####################################################################################################
+#                                           deploy.sh                                              #
+# This cript deploys the companyNews application from a Shell (Bash recommended).                  #
+# Make sure the WAR & ZIP file to deploy are in the assets folder.                                 #
+# The WAR file will be deployed to AWS Elastic Beanstalk.                                          #
+# The ZIP file will be unzipped and its content copied to a S3 bucket with public hosting enabled. #
+####################################################################################################
+
+exit 0
